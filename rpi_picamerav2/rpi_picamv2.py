3970c403917518a79fb02842131587f5343f29ef
develtechmon <lukasjohnnytechmon@gmail.com>
2023-02-20 17:58:16 +0000

    +added rpi picamv2 and userguide

---
+++ rpi_picamerav2/rpi_picamv2.py
@@ -0,0 +1,7 @@
+from picamera2 import Picamera2
+from libcamera import controls
+
+picam2 = Picamera2()
+picam2.start(show_preview=True)
+
+picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
